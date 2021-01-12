add269c54f3ab67fa93ea60280543446527a3f39
Danielle Arias <daniellearias321@gmail.com>
2021-01-12 15:07:21 -0800

    added styling

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ window.onload = function () {
     var buttonStop = document.getElementById("button-stop"); 
     var buttonReset = document.getElementById("button-reset"); 
     var Interval ; 
-}
 
 buttonStart.onclick = function() {
-    Interval(Interval); 
+
+    clearInterval(Interval); 
     Interval = setInterval(startTimer, 10); 
 }
 
@@ -23,7 +23,34 @@ buttonReset.onclick = function() {
     clearInterval(Interval); 
     tens= "00"; 
     seconds= "00"; 
-    appendsTens.innerHTML = tens; 
+    appendTens.innerHTML = tens; 
     appendSeconds.innerHTML = seconds; 
 }
+
+function startTimer () {
+    tens++; 
+
+    if(tens < 9) {
+        appendTens.innerHTML = "0" + tens; 
+    }
+
+    if (tens > 9) {
+        appendTens.innerHTML = tens; 
+    }
+
+    if (tens > 99) {
+        console.log("seconds"); 
+        seconds++; 
+        appendSeconds.innerHTML = "0" + seconds; 
+        tens = 0; 
+        appendTens.innerHTML = "0" + 0; 
+    }
+
+    if (seconds > 9){
+        appendSeconds.innerHTML = seconds; 
+    }
+}
+
+}
+
 
